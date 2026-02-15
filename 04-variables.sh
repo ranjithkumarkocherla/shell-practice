2d6a00b0ce97433190ac1baa159f5d3b58facb23
ranjith kumar <showriranjith@gmail.com>
2026-02-15 17:31:29 +0530

    var

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1 #no space between value and "="
+PERSON2=$2
+echo "$PERSON1:: Hello $PERSON2, how are you?"
+echo "$PERSON2:: I am good. thanks for asking. How are you doing?"
+echo "$PERSON1:: doing fine. Whar are you learning $PERSON1?"
+echo "$PERSON2:: I am learning shell. What about you?"
