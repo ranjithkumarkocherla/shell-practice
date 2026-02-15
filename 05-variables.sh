c5f8874399ff481ad0fa9d81dac2763619e48757
ranjith kumar <showriranjith@gmail.com>
2026-02-15 17:56:50 +0530

    dynamic parameter pass

--- 05-variables.sh
+++ 05-variables.sh
@@ -4,3 +4,6 @@ echo "Please enter your username::"
 read USER_NAME
 
 echo "user name is $USER_NAME"
+
+echo "Please enter your password::"
+read -s PASSWORD # -s hide the password or whatever you enter that doesnt visible on screen
