ff82410c8de16e6395688d858ca4e03a4cdbd090
ranjith kumar <showriranjith@gmail.com>
2026-02-15 15:36:42 +0530

    conversation

---
+++ 02-coversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh:: Hello ramesh, how are you?"
+echo "Ramesh:: I am good. thanks for asking. How are you doing?"
+echo "Suresh:: doing fine. Whar are you learning Ramesh?"
+echo "Ramesh:: I am learning shell. What about you?"
