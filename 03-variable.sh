7d6b3d4c24a8935520ee8537ba8278e3b8b9fe04
ranjith kumar <showriranjith@gmail.com>
2026-02-15 17:23:16 +0530

    var

--- 03-variable.sh
+++ 03-variable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Suresh #no space between value and "="
-PERSON2=Ramesh
+PERSON1=Puthin #no space between value and "="
+PERSON2=Trump
 echo "$PERSON1:: Hello $PERSON2, how are you?"
 echo "$PERSON2:: I am good. thanks for asking. How are you doing?"
 echo "$PERSON1:: doing fine. Whar are you learning $PERSON1?"
