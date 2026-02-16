c3849efd148cc45f9e31f7f400b12342c609da72
ranjith kumar <showriranjith@gmail.com>
2026-02-16 14:30:24 +0530

    data-types

---
+++ 10-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ] ; then 
+  echo "Please run this script with root user access"
+  exit 1
+fi
+
+  echo "Installing nginx"
+  dnf install nginx -y
+
+if [ $? -ne 0 ]; then 
+echo "installing nginx failure"
+exit 1
+else 
+echo "Installing nginx success"
+fi
+
