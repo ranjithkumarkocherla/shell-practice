320115fc1f5b14b94b7e6cad3f03467a739d3419
ranjith kumar <showriranjith@gmail.com>
2026-02-16 10:58:35 +0530

    dynamic parameter pass

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-STRT_TIME=$(date %s) #%s will calculate the time in seconds
+STRT_TIME=$(date +%s) #%s will calculate the time in seconds
 
 echo "script executed at: $STRT_TIME"
 
 sleep 10
 
-END_TIME=$(date %s)
+END_TIME=$(date +%s)
 
 EXE_DUR=$(($STRT_TIME-$END_TIME))
 
