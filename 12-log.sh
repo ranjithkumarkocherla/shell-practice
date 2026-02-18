17b45eb1b3df19f8ce54f8524b11bf1c9940baa1
ranjith kumar <showriranjith@gmail.com>
2026-02-18 14:30:24 +0530

    logs createing

--- 12-log.sh
+++ 12-log.sh
@@ -5,7 +5,7 @@ LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ] ; then 
-  echo "Please run this script with root user access"
+  echo "Please run this script with root user access" | tee -a $LOGS_FILE
   exit 1
 fi
 
@@ -14,10 +14,10 @@ mkdir -p $LOGS_FOLDER
 VALIDATE()
 {
    if [ $1 -ne 0 ]; then 
-echo "$2 failure"
+echo "$2 failure" | tee -a $LOGS_FILE
 exit 1
 else 
-echo "$2 success"
+echo "$2 success" | tee -a $LOGS_FILE
 fi
 }
 dnf install nginx -y &>> $LOGS_FILE
