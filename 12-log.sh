bad1af3f147be856b38d7fd08070f96988a39762
ranjith kumar <showriranjith@gmail.com>
2026-02-18 14:07:27 +0530

    nginx install

--- 12-log.sh
+++ 12-log.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGs_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ] ; then 
   echo "Please run this script with root user access"
   exit 1
 fi
+
+mkdir -p $LOGS_FOLDER
 
 VALIDATE()
 {
@@ -16,13 +20,11 @@ else
 echo "$2 success"
 fi
 }
-dnf install nginx -y
+dnf install nginx -y &>> $LOGS_FILE
 VALIDATE $? "installing nginx"
 
-dnf install mysql -y
+dnf install mysql -y $LOGS_FILE
 VALIDATE $? "install sql"
-
-dnf install nodejs -y
+ 
+dnf install nodejs -y $LOGS_FILE
 VALIDATE $? "install nodejs"
-
-  
