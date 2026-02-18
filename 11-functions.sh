f94a301901bc30c04b6cbfbcb2e16eeaf996b39d
ranjith kumar <showriranjith@gmail.com>
2026-02-18 13:27:37 +0530

    nginx install

---
+++ 11-functions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ] ; then 
+  echo "Please run this script with root user access"
+  exit 1
+fi
+
+VALIDATE()
+{
+   if [ $1 -ne 0 ]; then 
+echo "$2 failure"
+exit 1
+else 
+echo "$2 success"
+fi
+}
+dnf install nginx -y
+VALIDATE $? "installing nginx"
+
+dnf install mysql -y
+VALIDATE $? "install sql"
+
+dnf install nodejs
+VALIDATE $? "install nodejs"
+
+  
