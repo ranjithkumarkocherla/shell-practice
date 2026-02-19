06a9cb1bc426df7f4652e404a98c31ea8ad03c0e
ranjith kumar <showriranjith@gmail.com>
2026-02-19 12:48:52 +0530

    error handling_trap

---
+++ 18-trap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e #ERR
+trap 'echo "there is an error in $LINENO, command: $BASH_COMMAND"'
+
+echo "Hellow world"
+echo "I am learning Devops"
+echoo "printing error here"
+echo "No error in this"
