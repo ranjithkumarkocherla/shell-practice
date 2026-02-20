64ffb5b1db2c0741f9e1cf69eddb254ccdc8c961
ranjith kumar <showriranjith@gmail.com>
2026-02-20 14:48:31 +0530

    assignment_3

---
+++ assignment_3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#!/bin/bash
+
+# Define the strings
+OLD_STRING="FOO"
+NEW_STRING="BAR"
+FILE_PATTERN="*.conf"
+
+# Define the starting directory (current directory in this case)
+START_DIR="."
+
+echo "Replacing all occurrences of '$OLD_STRING' with '$NEW_STRING' in all $FILE_PATTERN files under $START_DIR"
+
+# Use find to locate files and sed to replace the string in-place
+find "$START_DIR" -name "$FILE_PATTERN" -type f -exec sed -i "s/$OLD_STRING/$NEW_STRING/g" {} +
+
+echo "Replacement complete."
