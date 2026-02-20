e4d50d5e57d0300ebbf88bc31e00aefad8683874
ranjith kumar <showriranjith@gmail.com>
2026-02-20 13:33:41 +0530

    assignment_1

---
+++ assignment_1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Write a script that accepts a filename (or path) as a command-line argument and 
+#prints whether:
+#The file exists
+#It is readable
+#It is writable
+#Or it is not present
+#Extension: If the argument is a directory, list all files inside it.
+
+# Check if an argument is provided
+if [ "$#" -ne 1 ]; then #$# = number of arguments passed to script.
+    echo "Usage: $0 <filename_or_path>"
+    exit 1
+fi
