7179e8d1bb31850e4cd0f789970db5aabb5a1bd2
ranjith kumar <showriranjith@gmail.com>
2026-02-20 15:50:14 +0530

    assignment_3

--- assignment_3.sh
+++ assignment_3.sh
@@ -9,9 +9,9 @@ FILE_PATTERN="*.conf"
 # Define the starting directory (current directory in this case)
 START_DIR="."
 
-echo "Replacing all occurrences of '$OLD_STRING' with '$NEW_STRING' in all  $FILE_PATTERN files under $START_DIR"
+#echo "Replacing all occurrences of '$OLD_STRING' with '$NEW_STRING' in all  $FILE_PATTERN files under $START_DIR"
 
 # Use find to locate files and sed to replace the string in-place
 FILES_TO_CHANGE=$(find "$START_DIR" -name "$FILE_PATTERN" -type f -exec sed -i "s/$OLD_STRING/$NEW_STRING/g" {} +)
 echo "Files to update: $FILES_TO_CHANGE"
-echo "Replacement complete."
+#echo "Replacement complete."
