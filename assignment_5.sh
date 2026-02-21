712d1f5572e6c6aee8a46564d110e46a13876414
ranjith kumar <showriranjith@gmail.com>
2026-02-21 13:50:20 +0530

    assignment_5

---
+++ assignment_5.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Task_5. Largest Number from Arguments
+#Write a script that finds the largest number from a list of numbers provided as command-line arguments.
+if [ "$#" -lt 1 ]; then
+    echo "Please enter the numbers: $0 numbers..."
+    exit 1
+fi
+
+largest=$1
+
+for num in "$@"
+do
+    if [ "$num" -gt "$largest" ]; then
+        largest=$num
+    fi
+done
+
+echo "Largest number is: $largest"
